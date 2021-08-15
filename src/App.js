58222a6fc4940df63bfaf7c2e4e50b6366cc53a9
Devin Dao-Tse <devindaotse@gmail.com>
2021-08-15 17:04:35 -0400

    added a lot of stuff from laptop

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React,{useState} from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 
 function App() {
+
+  const [usersList, setUsersList] = useState([])
+
+  const addUserHandler = (uName, uAge)=>{
+    setUsersList((prevUserList)=> {
+      return [...prevUserList, {id: Math.random().toString() ,name: uName, age:uAge}]
+    })
+  }
+
+
   return (
     <div>
-      <h2>Hello</h2>
-      <h2>Added to laptop</h2>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
     </div>
   );
 }
